fa6b5d84d9f010255e587213ba09672e38922178
Pkpattel <38481993+Pkpattel@users.noreply.github.com>
2021-11-23 08:39:08 -0600

    $default route file for AWS API Gateway websocket

---
+++ WebSocketDemo.py
@@ -0,0 +1,23 @@
+import json
+import boto3
+from random import randrange
+
+
+def lambda_handler(event, context):
+	client = boto3.resource('dynamodb')
+	table = client.Table('WebSocketTest')
+	
+	# generate random id for dynamodb table
+	RandomId = randrange(5)
+    Return RandomId
+	
+	#1. Example - Get Item By Id
+	response = table.get_item(
+		Key={
+			'Id' : RandomId
+		}
+	)
+	
+	return {
+		'body': response['Item']['NewValue']
+	}
